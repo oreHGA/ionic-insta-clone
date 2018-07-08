6044078b11e6f2d73e678707ee9f0ee130eb452e
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2018-07-08 19:41:34 +0000

    initial commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+
+  constructor() {
+
+  }
+}
