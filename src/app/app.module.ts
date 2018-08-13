9770970985376ecf73c2825a6c4631bd8bab7bee
Oreoluwa Ogundipe <oreogundipe@gmail.com>
2018-08-13 07:25:38 +0000

    setting up apollo client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+// import modules for apollo client
+import {HttpClientModule} from '@angular/common/http';
+import {ApolloModule,Apollo} from 'apollo-angular';
+import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+// import other pages
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ProfilePage } from '../pages/profile/profile';
+import { CommentPage } from '../pages/comment/comment';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { ProfilePage } from '../pages/profile/profile';
-import { CommentPage } from '../pages/comment/comment';
 
 @NgModule({
   declarations: [
@@ -20,6 +26,9 @@ import { CommentPage } from '../pages/comment/comment';
     CommentPage,
   ],
   imports: [
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -37,4 +46,14 @@ import { CommentPage } from '../pages/comment/comment';
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+
+
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink){
+    apollo.create({
+      link: httpLink.create({uri: 'http://localhost:4466'}), // uri specifies the endpoint for our graphql server
+      cache: new InMemoryCache()
+    })    
+  }
+
+}
